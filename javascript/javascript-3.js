d8cfe9c3af7c63d158ffbbec46cf87f47efe21be
efimova <efimova@betweenx.com>
2019-10-08 12:49:35 +0300

    .

--- javascript/javascript-3.js
+++ javascript/javascript-3.js
@@ -11,5 +11,21 @@ getPath($0) // => "..."
 */
 
 function getPath(element) {
-    
+    let tagName = element.tagName
+
+    // Иерархия
+    //     element.parentElement.parentElement //до html
+    // Уникальные атрибуты
+    //     element.id
+    //     element.classList
+    //     element.attributes
+
+    //     HIERARCHY = A stack of the element’s hierarchy, climbing up the DOM tree
+    //     For each element in the HIERARCHY analyse the following in sequence:
+    //     2.1 Check for an ID
+    //     2.2 Check for unique Attributes
+    //     2.3 Check Tags
+    //     2.4 Check sibling relations
+    //     3. Merge the resulting stack into a single CSS selector
+
 }
